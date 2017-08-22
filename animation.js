b1ade1cb590be5f5720d9eeb55c9a6b76e7d623c
elaine <1811000@stfrancisprep.org>
2017-08-22 09:59:47 -0400

    elaine's commit

--- animation.js
+++ animation.js
@@ -1,9 +1,9 @@
 function myMove() {
   var elem = document.getElementById("title");
   var pos = window.innerHeight;
-  var id = setInterval(frame, 5);
+  var id = setInterval(frame, 2);
   function frame() {
-    if (pos == 100) {
+    if (pos == 200) {
       clearInterval(id);
     } else {
       pos--;
